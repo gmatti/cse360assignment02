26ec57472f786218dd4fa3db82ecc76354b3a173
gmatti <60623436+gmatti@users.noreply.github.com>
2020-10-07 21:27:37 -0700

    initial commit of project

---
+++ cse360assignment02/src/cse360assignment02/AddingMachine.java
@@ -0,0 +1,63 @@
+package cse360assignment02;
+
+/**
+ * This class represents and adding machine (a calculator) 
+ * @author Ghassan Matti
+ *
+ */
+public class AddingMachine 
+{
+	
+	 private int total;
+	 
+	 /**
+	  * AddingMachine default constructor 
+	  */
+	 public AddingMachine () 
+	 {
+	    total = 0;  // not needed - included for clarity
+	 }
+	 
+	 /**
+	  * get the total  
+	  * @return the total
+	  */
+	 public int getTotal () 
+	 {
+	    return 0;
+	 }
+	 
+	 /**
+	  * adds the value
+	  * @param value
+	  */
+	 public void add (int value) 
+	 {
+		 
+	 }
+	 
+	 /**
+	  * subtracts the value
+	  * @param value
+	  */
+	 public void subtract (int value) 
+	 {
+		 
+	 }
+	 
+	 /**
+	  * returns the representation of the value
+	  */
+	 public String toString () 
+	 {
+	    return "";
+	 }
+	 
+	 /**
+	  * 
+	  */
+	 public void clear() 
+	 {
+		 
+	 }
+}
